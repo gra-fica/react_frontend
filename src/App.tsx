fd473530ad58473dfc637e3450c4abc26443d5ba
arch machine <lilith.alanis.255@gmail.com>
2024-07-09 18:37:16 -0600

    idk man

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,93 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { request } from 'http';
+import { RequestOptions } from 'https';
+
+interface Product{
+	id: number,
+	name: string,
+	price: number,
+	count: number,
+	cost: number,
+}
+
+interface Sale{
+	name: string,
+	price: number,
+	count: number,
+}
+
+
+function SaleRow({name, price, count}: Sale){
+	return (
+	<tr>
+		<td> <input defaultValue={name}/> </td>
+		<td> <input defaultValue={price}/> </td>
+		<td> <input defaultValue={count}/> </td>
+		<td> <input defaultValue={price * count}/> </td>
+	</tr>
+	);
+}
+
+const sales : Sale[] = [];
+
+enum InputState{
+	SettingProduct,
+	SettingPrice,
+	SettingCount,
+	AddProduct,
+}
+
+function Ticket(){
+	const [results, setResults] = useState("");
+
+	let ask_for = async (event : ChangeEvent) => {
+		let target = event.target as HTMLInputElement;
+		let r = await fetch(`/api/v1/product/search?name=${target.value}&total=10`, { method: "GET", });
+		let prods : {product: Product, score: number}[]= (await r.json()).products;
+		let best = prods[0].product.name;
+		setResults(best);
+		console.log(best);
+	};
+
+	return (
+	<div>
+		<table> <tbody>
+			<tr>
+				<th> name  </th>
+				<th> price </th>
+				<th> count </th>
+				<th> total </th>
+			</tr>
+			<tr>
+				<td> <input name="name"  id="name" onChange={ask_for}/>  </td>
+				<td> <input name="price" id="price"/> </td>
+				<td> <input name="count" id="count"/> </td>
+				<td> <input name="total" id="total"/> </td>
+			</tr>
+		</tbody> </table>
+		total
+		<div id='total_cost'> 0 </div>
+		<button> guardar </button>
+		<div> best match : {results} </div>
+	</div>);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+			<header>
+				<button onClick={() => {
+					let childs = document.getElementById('BOD')?.children!;
+				}}> New Ticket </button>
+				<button> Add Products </button>
+				<button> Login </button>
+				<button> Signin </button>
+			</header>
+			<Ticket />
+        </div>
+    );
 }
 
 export default App;
