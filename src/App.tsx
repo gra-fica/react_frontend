f1efa43390e2c31f9d3456b927c4c85c16d5828c
arch machine <lilith.alanis.255@gmail.com>
2024-12-19 20:04:30 -0600

    dd

--- src/App.tsx
+++ src/App.tsx
@@ -6,80 +6,75 @@ type DBPurchase = {
 	id  : number,
 	desc: string,
 	cost: number,
-	prepay: number,
-	client: number,
+	prepay?: number,
+	client?: number,
 };
 
-function PurchaseTable(purchases: DBPurchase[] | null){
-	let ps :any = <tr />;
 
-	if(purchases != null){
-		ps = purchases.map(p  => {
-			return <tr>
-				<td > <button> terminado </button> </td>
+
+function App() {
+	const [purchase, setpurhases] = useState<DBPurchase[]>([]);
+
+	let update_table = async () => {
+		try{
+			const response = await fetch('/api/v1/purchase/list', {method:'GET'});
+				if(!response.ok)
+					setpurhases([]);
+				else
+					setpurhases(await response.json() || []) ;
+		}
+		catch(error){
+			console.log(error)
+		}
+	};
+
+	const PurchaseTable = ( {purchases} : {purchases: DBPurchase[]}) => {
+		let ps = purchases.map(p  => {
+			return <tr key={p.id}>
+				<td > <button onClick={ _ => 
+					fetch(`/api/v1/purchase/delete/${p.id}`, { method:'DELETE' }).then( _ => update_table())
+				}> terminado </button> </td>
 				<td className="m-1 p-1"> {p.id} </td>
 				<td className="m-1 p-1"> {p.cost / 100.} </td>
 				<td className="m-1 p-1"> {p.desc} </td>
 			</tr>;
 		});
-	}
-	return <table className='Purchase'>
-		<tr className="bg-blue-300">
-			<th />
-			<th> ID </th>
-			<th> Precio </th>
-			<th className="w-96"> Desc </th>
-		</tr>
-		{ps}
-	</table>;
-}
-
-function App() {
-	const [purchases, setpurchases] = useState(PurchaseTable([]));
-
-	function update_table(){
-		fetch('/api/v1/purchase/list', {method:'GET'})
-			.then(r => {
-				if(r.ok)
-					return r.json();
-				return Promise.reject(new Error("err"));
-			})
-			.then(j => {
-				setpurchases(PurchaseTable( j as DBPurchase[] ))
-			})
+		return <table className='Purchase'>
+			<tr className="bg-blue-300">
+				<th />
+				<th> ID </th>
+				<th> Precio </th>
+				<th className="w-96"> Desc </th>
+			</tr>
+			{ps}
+		</table>;
 	}
 
-	function add_purchase(e: React.SyntheticEvent) {
-		e.preventDefault();
+	useEffect(() => { update_table() }, []);
 
-		const target = e.target as typeof e.target & {
-			desc: {value: string},
-			cost: {value: string},
-		};
-
-		const cost = Number.parseFloat(target.cost.value);
-		let bd = JSON.stringify({ cost: cost, desc: target.desc.value});
-		fetch('/api/v1/purchase/add', {
-			method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-			body: bd,
-		})
-
-		update_table()
-	}
-
-
-	// sure why not
-	useEffect( () => {
-		update_table()
-	}, []);
 
 	return (
 		<div className="App">
-			<div id="purchases"> {purchases} </div>
-			<form onSubmit={add_purchase}>
-				precio: <input id="cost" type="number"/>
-				descripcion: <input id="desc" type="text"/>
+			<PurchaseTable purchases={purchase} />
+			<form onSubmit={e => {
+				e.preventDefault();
+				const target = e.target as typeof e.target & {
+					desc: {value: string},
+					cost: {value: string},
+				};
+				const cost = Number.parseFloat(target.cost.value);
+
+				let bd = JSON.stringify({ cost: cost * 100, desc: target.desc.value});
+				fetch('/api/v1/purchase/add', {
+					method: 'POST',
+			    headers: { 'Content-Type': 'application/json' },
+					body: bd,
+				}).then(_ => update_table());
+
+				e.currentTarget.reset();
+			}}>
+				precio: <input id="cost" type="number" defaultValue={0} />
+				descripcion: <input id="desc" type="text" defaultValue={""} />
 				<button type='submit'> submit </button>
 			</form>
 		</div>
